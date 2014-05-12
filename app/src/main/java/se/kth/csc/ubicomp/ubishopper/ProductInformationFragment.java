0ef76ad7fc1fb2ad6eb1389a29c25efede327f5d
zenkig <zenkiggou@gmail.com>
2014-05-12 18:18:10 +0200

    Added small Products and Inmormation for wikitude POI view.

--- app/src/main/java/se/kth/csc/ubicomp/ubishopper/ProductInformationFragment.java
+++ app/src/main/java/se/kth/csc/ubicomp/ubishopper/ProductInformationFragment.java
@@ -48,28 +48,10 @@ public class ProductInformationFragment extends Fragment implements TabHost.OnTa
      *
      * @return A new instance of fragment ProductInformationFragment.
      */
-    // TODO: Rename and change types and number of parameters
-/*    public static ProductInformationFragment newInstance(String param1, String param2) {
-        ProductInformationFragment fragment = new ProductInformationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
+
     public ProductInformationFragment() {
         // Required empty public constructor
     }
-
-/*
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
